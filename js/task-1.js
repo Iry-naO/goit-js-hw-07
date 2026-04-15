97f987ef12f6992c9580273dbd95d49365c4394c
Iryna_Oksamytna <ira.strynadko@gmail.com>
2026-04-15 12:07:06 +0300

    task 1 done

---
+++ js/task-1.js
@@ -0,0 +1,21 @@
+// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+
+// Порахує й виведе в консоль кількість категорій в ul#categories,
+// тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль
+// текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Кількість категорій, їх назва та кількість елементів отримані
+// за допомогою властивостей і методів DOM-елементів
+// Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()
+
+const liItem = document.querySelectorAll(".item");
+const liItemLength = `Number of categories: ${liItem.length}`;
+console.log(liItemLength);
+
+liItem.forEach((item) => {
+  console.log(`Category: ${item.querySelector(".title").textContent}`);
+  console.log(`Elements: ${item.querySelectorAll(".subitem").length}`);
+});
